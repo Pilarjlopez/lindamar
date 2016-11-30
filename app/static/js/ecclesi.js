96928f29cfc76f2fe6c4d1722470df3cfb63865a
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2016-11-29 18:39:19 -0600

    barra fija y ecclesiapp

--- app/static/js/ecclesi.js
+++ app/static/js/ecclesi.js
@@ -13,6 +13,8 @@
     $('.button-collapse').sideNav({'edge': 'left'});
     $('.datepicker').pickadate({selectYears: 20});
     $('select').not('.disabled').material_select();
+      
+    $(window).stellar();
 
     var window_width = $(window).width();
 
@@ -24,13 +26,55 @@
             $('.parallax-container').addClass('hide');
             $('.menu').addClass('navbar-fixed');
             $('#contenido').css('margin-top', '600px');
-            $('#inicio a').replaceWith( '<a href="{{ url_for(' + 'curia.inicio' + ') }}" style="padding: 0 0 0 60px;"><img class="responsive-img" src="{{ url_for(' + 'static' + ', filename=' + 'img/escudo_curia.svg' + ') }}" style="width: 64px;height: 64px;"></a>' );
+            //$('#inicio a').replaceWith( '<a href="{{ url_for(' + 'curia.inicio' + ') }}" style="padding: 0 0 0 60px;"><img class="responsive-img" src="{{ url_for(' + 'static' + ', filename=' + 'img/escudo_curia.svg' + ') }}" style="width: 64px;height: 64px;"></a>' );
         } else {
             $('.parallax-container').removeClass('hide');
+            //$('.menu').css('margin-top', '0');
             $('.menu').removeClass('navbar-fixed');
             $('#contenido').css('margin-top', '0');
-            $('#inicio a').replaceWith( '<a href="{{ url_for(' + 'curia.inicio' + ') }}">Inicio</a>' );
+            //$('#inicio a').replaceWith( '<a href="{{ url_for(' + 'curia.inicio' + ') }}">Inicio</a>' );
         }
+    });
+
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+      
+    slide.waypoint(function (event, direction) {
+        dataslide = $(this).attr('data-slide');
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+    });
+ 
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
     });
 
   }); // end of document ready
