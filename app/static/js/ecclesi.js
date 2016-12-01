48867b35cdbd10542c3609e97fe79370d6b15ff6
Samuel Gutierrez <search.sama@gmail.com>
2016-12-01 11:08:07 -0600

    cambios locales

--- app/static/js/ecclesi.js
+++ app/static/js/ecclesi.js
@@ -1,8 +1,6 @@
 (function($){
   $(function(){
 
-    //var $SCRIPT_ROOT = {{ request.script_root|tojson|safe }};
-
     // Plugin initialization
     $('.carousel.carousel-slider').carousel({full_width: true});
     $('.carousel').carousel();
@@ -14,8 +12,6 @@
     $('.datepicker').pickadate({selectYears: 20});
     $('select').not('.disabled').material_select();
 
-    var window_width = $(window).width();
-
     $(window).scroll(function() {
         var altura_cabecera = $('.parallax-container').outerHeight(true);
         var altura_menu = $('.menu').outerHeight(true);
@@ -24,14 +20,16 @@
             $('.parallax-container').addClass('hide');
             $('.menu').addClass('navbar-fixed');
             $('#contenido').css('margin-top', '600px');
-            $('#inicio a').replaceWith( '<a href="{{ url_for(' + 'curia.inicio' + ') }}" style="padding: 0 0 0 60px;"><img class="responsive-img" src="{{ url_for(' + 'static' + ', filename=' + 'img/escudo_curia.svg' + ') }}" style="width: 64px;height: 64px;"></a>' );
+            //$('#inicio a').replaceWith( '<a href="{{ url_for(' + 'curia.inicio' + ') }}" style="padding: 0 0 0 60px;"><img class="responsive-img" src="{{ url_for(' + 'static' + ', filename=' + 'img/escudo_curia.svg' + ') }}" style="width: 64px;height: 64px;"></a>' );
         } else {
             $('.parallax-container').removeClass('hide');
             $('.menu').removeClass('navbar-fixed');
             $('#contenido').css('margin-top', '0');
-            $('#inicio a').replaceWith( '<a href="{{ url_for(' + 'curia.inicio' + ') }}">Inicio</a>' );
+            //$('#inicio a').replaceWith( '<a href="{{ url_for(' + 'curia.inicio' + ') }}">Inicio</a>' );
         }
     });
+
+    var window_width = $(window).width();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
