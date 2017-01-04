db2b1da54a643f144c1489ce9f6e20b6590e5d9c
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-01-04 16:42:41 -0600

    panel ecclesi inicio

--- app/mod_auth/forms.py
+++ app/mod_auth/forms.py
@@ -1,5 +1,5 @@
 # Formulario de importacion y RecaptchaField (opcional)
-from flask_wtf import FlaskForm # , RecaptchaField
+from flask_wtf import Form # , RecaptchaField
 
 # Los elementos de formulario de importacion como de campo de texto y BooleanField (opcional)
 from wtforms import TextField, PasswordField # BooleanField
@@ -9,6 +9,6 @@ from wtforms.validators import Required, Email, EqualTo
 
 
 # Definir el formulario de acceso (WTForms)
-class LoginForm(FlaskForm):
+class LoginForm(Form):
     email    = TextField('Email Address', [Email(), Required(message='Olvido su direccion de correo electronico?')])
     password = PasswordField('Password', [Required(message='Debe de proveer una clave. ;-)')]) 
